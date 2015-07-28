b0d764c8d88d4d41ddc1dd1f8ee1ad83bca7baf1
Nathan Wisnoski <wisnoski@indiana.edu>
2015-07-27 20:34:55 -0400

    modified write directory

--- analysis/2015-07-18_water-chem-analysis.R
+++ analysis/2015-07-18_water-chem-analysis.R
@@ -64,4 +64,4 @@ rownames(file.out) <- TN.out[,1]
 
 read.csv('./data/')
 
-write.table(file.out, './analysis/2015-07-27_water-chem-table.txt', sep='\t')
+write.table(file.out, './data/2015-07-27_water-chem-table.txt', sep='\t')
