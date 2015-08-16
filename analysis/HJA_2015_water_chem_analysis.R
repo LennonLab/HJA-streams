b405373850f81a1d0142981301e76c81772ed222
Nathan Wisnoski <wisnoski@indiana.edu>
2015-08-16 18:41:29 -0400

    update gitignore

--- analysis/HJA_2015_water_chem_analysis.R
+++ analysis/HJA_2015_water_chem_analysis.R
@@ -65,6 +65,4 @@ file.out <- cbind(TN.out[,3], TP.out[,3])
 colnames(file.out) <- c("TN", "TP")
 rownames(file.out) <- TN.out[,1]
 
-read.csv('./data/')
-
-write.table(file.out, './data/2015-07-27_water-chem-table.txt', sep='\t')
+write.table(file.out, './data/2015-07-27_water-chem-table.tsv', sep='\t')
