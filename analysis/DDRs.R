f80e3d73567959885055839d6d3fcc5643050521
Nathan Wisnoski <wisnoski@indiana.edu>
2016-07-06 17:40:29 -0400

    Updated DDR axis ticks to log-scale with non-log labels

--- analysis/DDRs.R
+++ analysis/DDRs.R
@@ -1,5 +1,6 @@
 # source("./analysis/InitialSetup.R")
 # source("./analysis/DistanceCalcs.R")
+# source("./analysis/Ordination.R")
 
 # Water Distance Decay
 water.xy <- xy[which(design$habitat == "water"),]
@@ -17,10 +18,10 @@ water.dists <- data.frame(water.den.dist.ls, water.geo.dist.ls,
                               water.struc.dist.ls, water.env.dist.ls)
 names(water.dists) <- c("den", "geo", "comm.struc", "env")
 
-water.lm <- (lm(log(water.dists$comm.struc) ~ water.dists$env))
-water.dd <- (lm(log(water.dists$comm.struc) ~ water.dists$geo))
-summary(water.lm)
-summary(water.dd)
+water.env.lm <- (lm(log(water.dists$comm.struc) ~ water.dists$env))
+water.geo.lm <- (lm(log(water.dists$comm.struc) ~ water.dists$geo))
+capture.output(summary(water.env.lm), file = "./tables/DDR_water-env.txt")
+capture.output(summary(water.geo.lm), file = "./tables/DDR_water-space.txt")
 
 # Sediment Distance Decay
 sed.xy <- xy[which(design$habitat == "sediment"),]
@@ -38,10 +39,10 @@ sed.dists <- data.frame(sed.den.dist.ls, sed.geo.dist.ls,
                             sed.struc.dist.ls, sed.env.dist.ls)
 names(sed.dists) <- c("den", "geo", "comm.struc", "env")
 
-sed.lm <- (lm(log(sed.dists$comm.struc) ~ sed.dists$env))
-sed.dd <- (lm(log(sed.dists$comm.struc) ~ sed.dists$geo))
-summary(sed.lm)
-summary(sed.dd)
+sed.env.lm <- (lm(log(sed.dists$comm.struc) ~ sed.dists$env))
+sed.geo.lm <- (lm(log(sed.dists$comm.struc) ~ sed.dists$geo))
+capture.output(summary(sed.env.lm), file = "./tables/DDR_sed-env.txt")
+capture.output(summary(sed.geo.lm), file = "./tables/DDR_sed-space.txt")
 
 # Headwaters DDRs
 headwater.env.dists <- vegdist(env.2[which(design$order < 2),], method = "bray")
@@ -51,88 +52,48 @@ headwater.geo.dists <- na.omit(liste(dist.mat[which(design$order < 2), which(des
 headwater.db <- vegdist(OTUsREL[which(design$order < 2),])
 headwater.dists <- cbind(liste((1 - headwater.db), entry = "comm.struc"), 
                          headwater.env.dists, headwater.geo.dists)
-headwater.lm <- (lm(log(headwater.dists$comm.struc) ~ headwater.dists$headwater.env.dists))
-headwater.dd <- (lm(log(headwater.dists$comm.struc) ~ headwater.dists$geo.dist))
-summary(headwater.lm)
-summary(headwater.dd)
+headwater.env.lm <- (lm(log(headwater.dists$comm.struc) ~ headwater.dists$headwater.env.dists))
+headwater.geo.lm <- (lm(log(headwater.dists$comm.struc) ~ headwater.dists$geo.dist))
+capture.output(summary(headwater.env.lm), file = "./tables/DDR_headwater-env.txt")
+capture.output(summary(headwater.geo.lm), file = "./tables/DDR_headwater-space.txt")
 
 # Higher Order DDRs
-mainstem.env.dists <- vegdist(env.2[which(design$order >= 2),], method = "bray")
-mainstem.env.dists <- liste(mainstem.env.dists, entry = "env")[,3]
-mainstem.geo.dists <- na.omit(liste(dist.mat[which(design$order >= 2), which(design$order >= 2)],
+downstream.env.dists <- vegdist(env.2[which(design$order >= 2),], method = "bray")
+downstream.env.dists <- liste(downstream.env.dists, entry = "env")[,3]
+downstream.geo.dists <- na.omit(liste(dist.mat[which(design$order >= 2), which(design$order >= 2)],
                                entry = "geo.dist"))
-mainstem.db <- vegdist(OTUsREL[which(design$order >= 2),])
-mainstem.dists <- cbind(liste((1 - mainstem.db), entry = "comm.struc"), 
-                        mainstem.env.dists, mainstem.geo.dists)
-mainstem.lm <- (lm(log(mainstem.dists$comm.struc) ~ mainstem.dists$mainstem.env.dists))
-mainstem.dd <- (lm(log(mainstem.dists$comm.struc) ~ mainstem.dists$geo.dist))
-summary(mainstem.lm)
-summary(mainstem.dd)
+downstream.db <- vegdist(OTUsREL[which(design$order >= 2),])
+downstream.dists <- cbind(liste((1 - downstream.db), entry = "comm.struc"), 
+                        downstream.env.dists, downstream.geo.dists)
+downstream.env.lm <- (lm(log(downstream.dists$comm.struc) ~ downstream.dists$downstream.env.dists))
+downstream.geo.lm <- (lm(log(downstream.dists$comm.struc) ~ downstream.dists$geo.dist))
+capture.output(summary(downstream.env.lm), file = "./tables/DDR_downstream-env.txt")
+capture.output(summary(downstream.geo.lm), file = "./tables/DDR_downstream-space.txt")
 
 
 ### Catchment Scale DDRs
 
-# # Principal Components Analysis on HJA Environment
-# hja.pca <- princomp(env.mat)
-# summary(hja.pca)
-# plot(hja.pca, type = "l")
-# biplot(hja.pca)
-# pc1 <- hja.pca$scores[,1]
-# pc2 <- hja.pca$scores[,2]
-# pc3 <- hja.pca$scores[,3]
-# 
-# cor(pc1, env.mat)
-# cor(pc2, env.mat)
-
-env.dists <- vegdist(env.2, method = "gower")
-env.dists <- liste(env.dists, entry = "env")[,3]
-geo.dists <- na.omit(liste(dist.mat, entry = "geo.dist"))
-#env.dists <- liste(dist(pc1), entry = "env")[,3]
-den.dists <- na.omit(liste(den.dists, entry = "dend.dist"))
-hja.dists <- cbind(liste(hja.db, entry = "comm.struc"), env.dists)
-hja.dists <- cbind(hja.dists, den.dists[,3], geo.dists[,3])
-names(hja.dists)[c(5,6)] <- c("den.dists", "geo.dists")
+hja.env.dists <- vegdist(env.2, method = "gower")
+hja.env.dists <- liste(hja.env.dists, entry = "env")[,3]
+hja.geo.dists <- na.omit(liste(dist.mat, entry = "geo.dist"))
+hja.den.dists <- na.omit(liste(den.dists, entry = "dend.dist"))
+hja.dists <- cbind(liste(hja.db, entry = "comm.struc"), hja.env.dists)
+hja.dists <- cbind(hja.dists, hja.den.dists[,3], hja.geo.dists[,3])
+names(hja.dists)[c(4,5,6)] <- c("env.dists", "den.dists", "geo.dists")
 
 hja.env.lm <- (lm(log(1-hja.dists$comm.struc) ~ hja.dists$env.dists))
 hja.den.lm <- (lm(log(1-hja.dists$comm.struc) ~ hja.dists$den.dists))
 hja.geo.lm <- (lm(log(1-hja.dists$comm.struc) ~ hja.dists$geo.dists))
 hja.geo_env.lm <- lm(log(1-hja.dists$comm.struc) ~ hja.dists$geo.dists * hja.dists$env.dists)
 
+AIC(hja.geo_env.lm)
+AIC(hja.env.lm)
+AIC(hja.geo.lm)
+
 summary(hja.geo_env.lm)
 summary(hja.env.lm)
 summary(hja.geo.lm)
 summary(hja.den.lm)
-
-# water.pca <- princomp(env.mat[which(design$habitat == "water"),2:6])
-# summary(water.pca)
-# water.pc1 <- water.pca$scores[,1]
-# cor(water.pc1, env.mat[which(design$habitat == "water"),2:6])
-# water.env.dists <- vegdist(env.2[which(design$habitat == "water"),], method = "gower")
-# water.env.dists <- liste(water.env.dists, entry = "env")[,3]
-# water.geo.dists <- na.omit(liste(dist.mat[which(design$habitat == "water"), which(design$habitat == "water")],
-#                                  entry = "geo.dist"))
-# #water.env.dists <- liste(dist(water.pc1), entry = "env")[,3]
-# water.dists <- cbind(liste((1 - water.db), entry = "comm.struc"), water.env.dists, water.geo.dists)
-# plot(water.dists$water.env.dists, log(water.dists$comm.struc))
-
-# sed.env.dists <- vegdist(env.2[which(design$habitat == "sediment"),], method = "gower")
-# sed.env.dists <- liste(sed.env.dists, entry = "env")[,3]
-# sed.pca <- princomp(env.mat[which(design$habitat == "sediment"),2:6])
-# summary(sed.pca)
-# sed.pc1 <- sed.pca$scores[,3]
-# cor(sed.pc1, env.mat[which(design$habitat == "sediment"),2:6])
-# sed.geo.dists <- na.omit(liste(dist.mat[which(design$habitat == "sediment"), which(design$habitat == "sediment")],
-#                                entry = "geo.dist"))
-# #sed.env.dists <- liste(dist(sed.pc1), entry = "env")[,3]
-# sed.dists <- cbind(liste((1-sediment.db), entry = "comm.struc"), sed.env.dists, sed.geo.dists)
-# plot(sed.dists$sed.env.dists, log(sed.dists$comm.struc))
-
-
-plot(sed.dists$geo.dist, log(sed.dists$comm.struc))
-plot(water.dists$geo.dist, log(water.dists$comm.struc))
-
-summary(sed.dd)
-summary(water.dd)
 
 ################# FIGURES
 
@@ -148,22 +109,26 @@ plot(headwater.dists$headwater.env.dists,
      ylab = "", xaxt="n", yaxt="n", xlim = c(0,.5))
 abline(headwater.lm, lty = 1, lwd = 2)
 axis(side=1, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd = 2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.2)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.2)
 
 par(mar = c(4, 5, 1, 0) + 0.4)
 plot(mainstem.dists$mainstem.env.dists, 
      log(mainstem.dists$comm.struc), xlab="", 
      ylab = "", xaxt = "n", yaxt = "n", xlim = c(0,.5))
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd=2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.2)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.2)
 abline(mainstem.lm, lwd = 2)
 mtext("Environmental Distance", side = 1, line = 3, cex = 1.5)
 
@@ -173,10 +138,10 @@ plot(headwater.dists$geo.dist,
      ylab = "", xaxt="n", yaxt="n", xlim = c(0,12000))
 abline(headwater.dd, lty = 1, lwd = 2)
 axis(side=1, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-mtext("Headwaters", side = 4, line = 1, cex = 1.2)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
+mtext("Headwaters", side = 4, line = 1.5, cex = 1.2)
 box(lwd = 2)
 
 par(mar = c(4, 1, 1, 4) + 0.4)
@@ -184,13 +149,13 @@ plot(mainstem.dists$geo.dist,
      log(mainstem.dists$comm.struc), xlab="", 
      ylab = "", xaxt = "n", yaxt = "n", xlim = c(0,12000))
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd=2)
 abline(mainstem.dd, lwd = 2)
 mtext("Geographic Distance", side = 1, line = 3, cex = 1.5)
-mtext("Higher Orders", side = 4, line = 1, cex = 1.2)
+mtext("Higher Orders", side = 4, line = 1.5, cex = 1.2)
 
 dev.off()
 graphics.off()
@@ -212,22 +177,26 @@ plot(water.dists$env,
      ylab = "", xaxt="n", yaxt="n", xlim = c(0,.6))
 abline(water.lm, lty = 1, lwd = 2)
 axis(side=1, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd = 2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.2)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.2)
 
 par(mar = c(4, 5, 1, 0) + 0.4)
 plot(sed.dists$env, 
      log(sed.dists$comm.struc), xlab="", 
      ylab = "", xaxt = "n", yaxt = "n", xlim = c(0,.6))
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd=2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.2)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.2)
 abline(sed.lm, lwd = 2)
 mtext("Environmental Distance", side = 1, line = 3, cex = 1.5)
 
@@ -237,10 +206,10 @@ plot(water.dists$geo,
      ylab = "", xaxt="n", yaxt="n", xlim = c(0,12000))
 abline(water.dd, lty = 1, lwd = 2)
 axis(side=1, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-mtext("Waters", side = 4, line = 1, cex = 1.2)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
+mtext("Bacterioplankton", side = 4, line = 1.5, cex = 1.2)
 box(lwd = 2)
 
 par(mar = c(4, 1, 1, 4) + 0.4)
@@ -248,13 +217,13 @@ plot(sed.dists$geo,
      log(sed.dists$comm.struc), xlab="", 
      ylab = "", xaxt = "n", yaxt = "n", xlim = c(0,12000))
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd=2)
 abline(sed.dd, lwd = 2)
 mtext("Geographic Distance", side = 1, line = 3, cex = 1.5)
-mtext("Sediment", side = 4, line = 1, cex = 1.2)
+mtext("Sediment Bacteria", side = 4, line = 1.5, cex = 1.2)
 
 dev.off()
 graphics.off()
@@ -266,7 +235,7 @@ grid.raster(img)
 
 
 # Catchment-Scale DDRs
-png(filename = "./figures/Figure10.png",
+png(filename = "./figures/DDR_HJA_com-env.png",
     width = 1200, height = 1200, res = 96*2)
 
 par(mar = c(5, 5, 3, 3) + 0.4)
@@ -274,34 +243,37 @@ plot(hja.dists$env.dists, log(1 - hja.dists$comm.struc), xlab="",
      ylab = "", xaxt="n", yaxt="n")
 abline(hja.env.lm, lwd = 2)
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd = 2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.5)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.5)
 mtext("Environmental Distance", side = 1, line = 3, cex = 1.5)
 dev.off()
 graphics.off()
 
-png(filename = "./figures/Figure11.png",
+png(filename = "./figures/DDR_HJA_com-den.png",
     width = 1200, height = 1200, res = 96*2)
-grid.raster(readPNG("./figures/Figure10.png"))
 
 par(mar = c(5, 5, 3, 3) + 0.4)
 plot(hja.dists$den.dists, log(1 - hja.dists$comm.struc), xlab="", 
      ylab = "", xaxt="n", yaxt="n")
 abline(hja.den.lm, lwd = 2)
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd = 2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.5)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.5)
 mtext("Dendritic distance (m)", side = 1, line = 3, cex = 1.5)
 dev.off()
 graphics.off()
 
-png(filename = "./figures/Figure12.png",
+png(filename = "./figures/DDR_HJA_com-geo.png",
     width = 1200, height = 1200, res = 96*2)
 
 par(mar = c(5, 5, 3, 3) + 0.4)
@@ -309,16 +281,18 @@ plot(hja.dists$geo.dists, log(1 - hja.dists$comm.struc), xlab="",
      ylab = "", xaxt="n", yaxt="n")
 abline(hja.geo.lm, lwd = 2)
 axis(side=1, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=2, labels=T, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=2, 
+     labels=c(".1", ".2", ".3", ".4", ".5", ".6", ".7", ".8", ".9", "1.0"), 
+     at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 axis(side=3, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
-axis(side=4, labels=F, lwd.ticks=2, cex.axis=1.2, las=1)
+axis(side=4, labels=F, at=log(seq(0.1:1, by = 0.1)), lwd.ticks=2, cex.axis=1.2, las=1)
 box(lwd = 2)
-mtext("log Community Similarity", side = 2, line = 3, cex = 1.5)
+mtext("Community Similarity", side = 2, line = 3, cex = 1.5)
 mtext("Geographic Distance (m)", side = 1, line = 3, cex = 1.5)
 dev.off()
 graphics.off()
 
-png(filename = "./figures/Figure13.png",
+png(filename = "./figures/DDR_HJA_env-geo.png",
     width = 1200, height = 1200, res = 96*2)
 
 par(mar = c(5, 5, 3, 3) + 0.4)
@@ -335,7 +309,7 @@ mtext("Geographic Distance (m)", side = 1, line = 3, cex = 1.5)
 dev.off()
 graphics.off()
 
-png(filename = "./figures/Figure14.png",
+png(filename = "./figures/DDR_HJA_env-den.png",
     width = 1200, height = 1200, res = 96*2)
 
 par(mar = c(5, 5, 3, 3) + 0.4)
--- analysis/DistanceCalcs.R
+++ analysis/DistanceCalcs.R
@@ -58,4 +58,8 @@ dist.mat[!lower.tri(dist.mat)] <- NA
 den.dists <- dend.dist.mat[which(rownames(dend.dist.mat) %in% rownames(design)),
                        which(rownames(dend.dist.mat) %in% rownames(design))]
 den.dists[!lower.tri(den.dists)] <- NA 
-env.2 <- env.mat[,2:6]+2.685105 # Make all values positive
+env.2 <- env.mat[,2:6]
+habitat <- scale((env[,8] == "sediment") * 1)
+env.2 <- as.data.frame(cbind(habitat, env.2))
+colnames(env.2)[1] <- "habitat"
+env.2 <- env.2 + abs(min(env.2))
