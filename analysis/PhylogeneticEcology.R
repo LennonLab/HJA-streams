649e78c587d1da2bba553b68105dcc287bffdb49
Nathan Wisnoski <wisnoski@indiana.edu>
2016-10-11 23:29:42 -0400

    fixed otu rep fasta

--- analysis/PhylogeneticEcology.R
+++ analysis/PhylogeneticEcology.R
@@ -4,6 +4,3 @@ hja.tree <- read.tree(file = "./data/hja_streams.tree")
 
 matched.phylo <- match.phylo.comm(hja.tree, OTUs)
 
-cophenetic(matched.phylo$phy)
-
-pd(samp = matched.phylo$comm, tree = matched.phylo$phy, include.root = F)
