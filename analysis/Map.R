7a0fbe1538e03620650401869453e2130b9d7dd3
Nathan Wisnoski <wisnoski@indiana.edu>
2017-07-30 18:10:02 -0400

    updated map making

--- analysis/Map.R
+++ analysis/Map.R
@@ -20,15 +20,8 @@ latrange <- c()
 basemap <- get_map(location = c(-122.3, 44.2, -122.1, 44.3), 
                    zoom = 12, maptype = "terrain")
 map1 <- ggmap(basemap)
-map1 + geom_point(data = env, 
+hja.map <- map1 + geom_point(data = env, 
                   aes(x = longitude, y = latitude, cex = 2), 
-                  color = "red", show.legend = F) +
-  geom_polygon()
+                  color = "black", shape = 21, fill = "red", show.legend = F) +
   scale_x_continuous(limits = c(-122.27, -122.10)) +
   scale_y_continuous(limits = c(44.18, 44.30))
-
-streamlines <- readShapeSpatial("imagery/lidar_stream/lidar_stream.shp", 
-                 proj4string = CRS("+proj=longlat +datum=WGS84"))
-streamlines <- project(streamlines, "+proj=longlat")
-data <- fortify(streamlines)
-ggmap(map1)
