49c07ab65272596746028cb41736631e31ffee23
Nathan Wisnoski <wisnoski@indiana.edu>
2017-07-30 18:10:58 -0400

    updated reqs for map making

--- analysis/Map.R
+++ analysis/Map.R
@@ -1,3 +1,5 @@
+source("analysis/InitialSetup.R")
+
 require(ggplot2)
 require(ggmap)
 require(maps)
@@ -9,8 +11,6 @@ require(sp)
 require(raster)
 require(rgdal)
 require(dismo)
-
-source("analysis/InitialSetup.R")
 
 range(env$latitude)
 range(env$longitude)
@@ -24,4 +24,7 @@ hja.map <- map1 + geom_point(data = env,
                   aes(x = longitude, y = latitude, cex = 2), 
                   color = "black", shape = 21, fill = "red", show.legend = F) +
   scale_x_continuous(limits = c(-122.27, -122.10)) +
-  scale_y_continuous(limits = c(44.18, 44.30))
+  scale_y_continuous(limits = c(44.18, 44.30))+
+  labs(x="Longitude (dec. degrees)", y="Latitude (dec. degrees)")+
+  theme(axis.title = element_text(size = 16), axis.text = element_text(size = 12))
+hja.map
