f11079956a4880cde82b5451cab7df8013afbe09
Nathan Wisnoski <wisnoski@indiana.edu>
2018-12-17 23:59:43 -0500

    updated 16s tree

--- mothur/hja_streams.sh
+++ mothur/hja_streams.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #PBS -k o
-#PBS -l nodes=1:ppn=8,vmem=250gb,walltime=168:00:00
+#PBS -l nodes=1:ppn=8,vmem=50gb,walltime=168:00:00
 #PBS -M wisnoski@indiana.edu
 #PBS -m abe
 #PBS -j oe
