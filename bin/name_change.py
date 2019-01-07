fbb0d1ae07c99114b51fb8ff3a369fc243e0f9cc
Nathan Wisnoski <wisnoski@indiana.edu>
2019-01-07 14:09:00 -0500

    changed otu labels from 6 to 5 characters

--- bin/name_change.py
+++ bin/name_change.py
@@ -35,7 +35,7 @@ for seq_record in SeqIO.parse(in_file, "fasta"):
     OldseqID.append(seq_record.id)
     seqInfo.append(seq_record.description)
     seqID_New = seq_record.description
-    seqID_New = re.findall('Otu.{6}',seq_record.description)
+    seqID_New = re.findall('Otu.{5}',seq_record.description)
     NewseqID.append(seqID_New)
     seq_record.id = seqID_New[0]
 
