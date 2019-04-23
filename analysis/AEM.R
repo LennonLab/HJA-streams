49daa48ea64aa1eefa39028a376fd084a2ccb25a
Nathan Wisnoski <wisnoski@indiana.edu>
2019-04-23 19:22:55 -0400

    scale dependence of beta, and updated some figs to be more consistent

--- analysis/AEM.R
+++ analysis/AEM.R
@@ -86,61 +86,62 @@ envfit(sed.spatial1, sed.eigs)
 
 sed.space.mod <- model.matrix(sed.spatial1)
 
-sed.env <- as.data.frame(env.mat[which(design$habitat == "sediment"),-c(1,2)])
+sed.env <- as.data.frame(env.subs[which(design$habitat == "sediment"),-c(1,2)])
 sed.env.mod <- model.matrix(~ .,sed.env)[,-1]
-varpart(sed.db, sed.env.mod, sed.space.mod)
+sed.space.mod <- model.matrix(~ ., sed.aem.vec[,1:11])[,-1]
+varpart(sed.OTUsREL.hel, sed.env.mod, sed.space.mod)
 
 
-# construct water site-by-edge matrix
-water.E <- matrix(0, nrow = 32, ncol = 40)
-rownames(water.E) <- rownames(design.water)
-water.E["LC_02_W",c(1:24)] <- 1
-water.E["LC_03_W",c(2,5:9)] <- 1
-water.E["LC_04_W",c(5:9)] <- 1
-water.E["LC_05_W",c(10:15,17:24)] <- 1
-water.E["LC_07_W",c(8,9)] <- 1
-water.E["LC_08_W",6] <- 1
-water.E["LC_09_W",11] <- 1
-water.E["LC_10_W",13] <- 1
-water.E["LC_11_W",9] <- 1
-water.E["LC_13_W",c(18:24)] <- 1
-water.E["LC_14_W",15] <- 1
-water.E["LC_16_W",16] <- 1
-water.E["LC_18_W",c(23,24)] <- 1
-water.E["LC_20_W",24] <- 1
-water.E["LC_21_W",21] <- 1
-water.E["LC_22_W",19] <- 1
-water.E["W1_02_W",c(25:40)] <- 1
-water.E["W1_03_W",c(26:40)] <- 1
-water.E["W1_21_W",c(27:40)] <- 1
-water.E["W1_22_W",c(28:40)] <- 1
-water.E["W1_04_W",c(29:40)] <- 1
-water.E["W1_05_W",c(30:40)] <- 1
-water.E["W1_06_W",c(31:40)] <- 1
-water.E["W1_08_W",32] <- 1
-water.E["W1_11_W",c(34:40)] <- 1
-water.E["W1_12_W",c(35:40)] <- 1
-water.E["W1_13_W",c(36:40)] <- 1
-water.E["W1_14_W",c(37:40)] <- 1
-water.E["W1_15_W",c(38:40)] <- 1
-water.E["W1_17_W",c(39:40)] <- 1
-water.E["W1_20_W",40] <- 1
+# # construct water site-by-edge matrix
+# water.E <- matrix(0, nrow = 32, ncol = 40)
+# rownames(water.E) <- rownames(design.water)
+# water.E["LC_02_W",c(1:24)] <- 1
+# water.E["LC_03_W",c(2,5:9)] <- 1
+# water.E["LC_04_W",c(5:9)] <- 1
+# water.E["LC_05_W",c(10:15,17:24)] <- 1
+# water.E["LC_07_W",c(8,9)] <- 1
+# water.E["LC_08_W",6] <- 1
+# water.E["LC_09_W",11] <- 1
+# water.E["LC_10_W",13] <- 1
+# water.E["LC_11_W",9] <- 1
+# water.E["LC_13_W",c(18:24)] <- 1
+# water.E["LC_14_W",15] <- 1
+# water.E["LC_16_W",16] <- 1
+# water.E["LC_18_W",c(23,24)] <- 1
+# water.E["LC_20_W",24] <- 1
+# water.E["LC_21_W",21] <- 1
+# water.E["LC_22_W",19] <- 1
+# water.E["W1_02_W",c(25:40)] <- 1
+# water.E["W1_03_W",c(26:40)] <- 1
+# water.E["W1_21_W",c(27:40)] <- 1
+# water.E["W1_22_W",c(28:40)] <- 1
+# water.E["W1_04_W",c(29:40)] <- 1
+# water.E["W1_05_W",c(30:40)] <- 1
+# water.E["W1_06_W",c(31:40)] <- 1
+# water.E["W1_08_W",32] <- 1
+# water.E["W1_11_W",c(34:40)] <- 1
+# water.E["W1_12_W",c(35:40)] <- 1
+# water.E["W1_13_W",c(36:40)] <- 1
+# water.E["W1_14_W",c(37:40)] <- 1
+# water.E["W1_15_W",c(38:40)] <- 1
+# water.E["W1_17_W",c(39:40)] <- 1
+# water.E["W1_20_W",40] <- 1
 
+waterSBE <- read_csv("data/AEMwater_site-by-edge.csv")
+waterOTUsREL.hel <- OTUsREL.hel[which(design$habitat == "water"),]
+wateraem <- aem(binary.mat = as.matrix(waterSBE[,-1]))
+matplot(wateraem$vectors, type = 'l')
+plot(wateraem$values/sum(wateraem$values))
+wateraem.vec <- as.data.frame(wateraem$vectors)
+waterdb <- vegdist(waterOTUsREL.hel, method = "euclidean")
+wateraem.full <- rda(waterOTUsREL.hel ~ ., wateraem.vec[,1:(floor(ncol(wateraem.vec)/2))])
+(waterr2.aem <- RsquareAdj(wateraem.full)$adj.r.squared)
+wateraem.fwd <- forward.sel(waterOTUsREL.hel, wateraem.vec)
+wateraem.sig.vec <- wateraem.vec[,c(sort(wateraem.fwd[,2]))]
+wateraem.sig <- rda(waterOTUsREL.hel ~ ., as.data.frame(wateraem.sig.vec))
+anova(wateraem.sig)
 
-water.db <- vegdist(OTUsREL[which(design$habitat == "water"),])
-water.aem <- cmdscale(dist(water.E), eig = T, k = 31)
-plot(water.aem$eig/sum(water.aem$eig))
-water.aem.eigvec <- as.data.frame(water.aem$points)
-mod1 <- dbrda(water.db ~ ., data = water.aem.eigvec)
-envfit(mod1, water.aem.eigvec)
-water.eigs <- subset(water.aem.eigvec, select = c(V1, V2, V6, V10, V27))
-water.spatial0 <- dbrda(water.db ~ 1, water.eigs)
-water.spatial1 <- dbrda(water.db ~ ., water.eigs)
-water.spatial <- ordiR2step(water.spatial0, water.spatial1)
-plot(water.spatial1)
-water.space.mod <- model.matrix(water.spatial1)
-
-water.env <- as.data.frame(env.mat[which(design$habitat == "water"),-c(1,2)])
+water.space.mod <- model.matrix(~ ., wateraem.vec[,1:(floor(ncol(wateraem.vec)/2))])[,-1]
+water.env <- as.data.frame(env.subs[which(design$habitat == "water"),-c(1,2)])
 water.env.mod <- model.matrix(~ .,water.env)[,-1]
-varpart(water.db, water.env.mod, water.space.mod)
-?varpart
+varpart(Y = waterOTUsREL.hel, as.data.frame(water.env.mod), as.data.frame(water.space.mod))
